9a2534739d9dd6b08b9558f5c0c5617c55362704
Your Name <risknc@gmail.com>
2018-12-03 17:47:35 -0500

    velocity profile python scriptstarted

---
+++ vel_pro.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""Q5_Data_Processing.py: Processes data set per question 5 in Test Engineer Quiz.pdf"""
+
+
+___author___ = "Kyle Cothern"
+___copyright___ = "Copyright 2018"
+___credits = ["Kyle Cothern"]
+___license___ = "GPL"
+___version___ = "0.1.0"
+___maintainer___= ""
+___email___= "risknc@gmail.com"
+___status___="Beta"
+
